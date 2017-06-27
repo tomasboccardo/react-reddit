82db5db74297a58f2574cf66bd02085fb5768572
Tomas Boccardo <tboccardo@medallia.com>
2017-06-27 15:33:58 -0300

    Refactor App to a stateless component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,19 +7,15 @@ import Header from './components/header/Header';
 
 import './App.css';
 
-class App extends React.Component {
-	render() {
-		return (
-			<div className="App">
-				<Header/>
-				<div className="App__body">
-					<Switch>
-						{routes.map(route => <Route exact={route.exact} key={route.path} path={route.path} component={route.component}/>)}
-					</Switch>
-				</div>
+const App = () => (
+		<div className="App">
+			<Header/>
+			<div className="App__body">
+				<Switch>
+					{routes.map(route => <Route exact={route.exact} key={route.path} path={route.path} component={route.component}/>)}
+				</Switch>
 			</div>
-		);
-	}
-}
+		</div>
+	);
 
 export default App
