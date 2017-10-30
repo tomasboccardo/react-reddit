086e7f15526f588b5a7a97323bdd69ec93532dcc
Tomas Boccardo <tboccardo@medallia.com>
2017-10-30 11:30:18 -0300

    Adding documentation to server code

--- server/middlewares.js
+++ server/middlewares.js
@@ -11,32 +11,35 @@ import routes from '../src/routes';
 import configureStore from '../src/store';
 
 import App from '../src/components/app/App'
+import index from '../build/index.html';
 
 function _objectToSanitizeJSON(obj) {
 	return JSON.stringify(obj).replace(/</g, '\\u003c');
 }
 
-function _universalRenderer(req, res, index) {
+export function universalRenderer(req, res) {
+	// We need to create a new store for each request.
 	const history = createMemoryHistory();
-
-	let store = configureStore({ history });
+	const store = configureStore({ history });
 
 	const promises = [];
 	routes.some(route => {
 		const match = matchPath(req.url, route);
 		if (match) {
+			// For each route that matches, we will fetch the initial data to populate the newly created store.
 			promises.push(route.component.withInitialDataFetch(store, match.params));
 		}
 		return match
 	});
 
 	Promise.all(promises).then(() => {
+		// Once all the initial data is fetched, we need to render the React App to a string to generate the page HTML
 		const context = {};
 		const state = _objectToSanitizeJSON(store.getState());
 		const reactApp = ReactDomServer.renderToString(
 			<Provider store={store}>
 				<StaticRouter context={context} location={req.url}>
-					<App/>
+					<App />
 				</StaticRouter>
 			</Provider>
 		);
@@ -45,22 +48,17 @@ function _universalRenderer(req, res, index) {
 			res.redirect(302, context.url);
 		} else {
 			const renderedApp = index
-				.replace('<div id="root"></div>', `<div id="root">${reactApp}</div>`)
-				.replace('<script id="state"></script>',
-					`<script id="state">window.__PRELOADED_STATE__=${state}</script>`);
+				// We need to embed the React App HTML that we geneated
+				.replace(
+					'<div id="root"></div>',
+					`<div id="root">${reactApp}</div>`
+				)
+				// Also, we need to send the current state of the App to avoid flickering
+				.replace(
+					'<script id="state"></script>',
+					`<script id="state">window.__PRELOADED_STATE__=${state}</script>`
+				);
 			res.send(renderedApp)
 		}
 	})
-}
-
-export function universalRenderer(req, res) {
-	fs.readFile(path.resolve(__dirname, '..', 'build', 'index.html'), 'utf8', function (err, index) {
-		if (err) {
-			console.log(err);
-			res.status(500).send(err)
-		} else {
-			_universalRenderer(req,res, index)
-		}
-	});
-
 }
